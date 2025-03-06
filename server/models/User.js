d6cc559b8f1b5da72173e56aa4a61d893d10f209
Manish Anwla <2022csb1090@iitrpr.ac.in>
2025-03-07 00:11:33 +0530

    updating user.js

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,13 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isVerified: { type: Boolean, default: false }
+    isVerified: { type: Boolean, default: false },
+    mobileNo: { type: Number, required: false },
+    DOB: { type: Date, required: false },
+    Degree: { type: String, required: false },
+    Department: { type: String, required: false },
+    CurrentYear: { type: Number, required: false },
+    ProfilePic: { type: String, required: false },
 });
 
 const User = mongoose.model('User', userSchema);
