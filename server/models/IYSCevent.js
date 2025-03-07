9b63a90a257da562e4c6afc0b8775beb47eac8fc
Manish Anwla <2022csb1090@iitrpr.ac.in>
2025-03-07 12:33:49 +0530

    new model create for iysc event

---
+++ server/models/IYSCevent.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const IYSCeventSchema = new mongoose.Schema({
+    type: { type: String, required: true },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    venue: { type: String, required: true },
+    description: { type: String, required: true },
+    winner: { type: String, required: false },
+});
+
+const IYSCevent = mongoose.model('IYSCevent', IYSCeventSchema);
+
+export default IYSCevent;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,6 +4,7 @@ import { verify_email } from '../controllers/verify_email.js';
 import { login } from '../controllers/login.js';
 import { getProfile } from '../controllers/profile.js'; // Import the getProfile controller
 import { updateProfile } from '../controllers/editProfile.js';
+import { addIYSCevent } from '../controllers/addIYSCevent.js';
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.post("/verify-email", verify_email);
 router.post("/login", login);
 router.get("/profile", getProfile); // Ensure this is a GET request
 router.patch("/update-profile", updateProfile);
+router.post("/add-IYSCevent", addIYSCevent);
 
 export default router;
