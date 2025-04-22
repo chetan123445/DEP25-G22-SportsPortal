eb108e7b1f77575235cf0d3ef862575a364dda88
chetan123445 <2022csb1074@iitrpr.ac.in>
2025-04-22 23:49:13 +0530

    logo

--- sp_frontend/lib/constants.dart
+++ sp_frontend/lib/constants.dart
@@ -1,1 +1,1 @@
-const String baseUrl = 'http://localhost:5000'; // Ensure this matches your backend server's address
+const String baseUrl = 'http://34.131.63.205:5000'; // Ensure this matches your backend server's address
